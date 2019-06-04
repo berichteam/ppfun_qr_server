3fa5ad758ad77f6b6f69db6e7690c36d78d4e285
Goodwin <swim1986@126.com>
2019-06-04 16:18:27 +0800

    修改服务权限

--- config/run.sh
+++ config/run.sh
@@ -5,6 +5,6 @@ home=/usr
 app_dir=/Users/Superwen/pythonprojects/ppfun_qr_server
 
 
-$uwsgi --uid nobody --gid nobody --chdir $app_dir --http :8011 -M  -p 1 -w server --callable app -t 60 --max-requests 5000 --vacuum --home $home --daemonize /tmp/ppfun_qr_server.log --pidfile /tmp/ppfun_qr_server.pid --touch-reload /tmp/ppfun_qr_server.touch
+$uwsgi --uid root --gid root --chdir $app_dir --http :8011 -M  -p 1 -w server --callable app -t 60 --max-requests 5000 --vacuum --home $home --daemonize /tmp/ppfun_qr_server.log --pidfile /tmp/ppfun_qr_server.pid --touch-reload /tmp/ppfun_qr_server.touch
 
 
